da820659baf3e7f7760f8c14c9d2873f15cf9aab
Aprizotic <kajusbut24@gmail.com>
2025-11-18 12:41:56 +0000

    complete desktop design

--- news-homepage-main/script.js
+++ news-homepage-main/script.js
@@ -1,10 +1,31 @@
 const openButton = document.querySelector('[data-js="open-modal"]');
 const closeButton = document.querySelector('[data-js="close-modal"]');
 const navMenu = document.querySelector('.header__nav-items');
+const navItems = document.querySelectorAll('.header__nav-item');
+const mediaQuery = window.matchMedia('(min-width: 40rem)');
+
+const adjustDevice = (e) => {
+  if (e.matches) {
+    openButton.classList.add('hidden');
+    navMenu.classList.add('header__nav-items--desktop');
+    navMenu.classList.remove('hidden');
+    navItems.forEach((item) => {
+      item.classList.add('header__nav-item--desktop');
+    });
+  } else {
+    openButton.classList.remove('hidden');
+    navMenu.classList.remove('header__nav-items--desktop');
+    navMenu.classList.add('hidden');
+    navItems.forEach((item) => {
+      item.classList.remove('header__nav-item--desktop');
+    });
+  }
+}
 
 const toggleMenu = () => {
   navMenu.classList.toggle('hidden');
   closeButton.classList.toggle('hidden');
+  openButton.classList.toggle('hidden');
 }
 
 openButton.addEventListener('click', () => {
@@ -14,3 +35,7 @@ openButton.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
   toggleMenu();
 });
+
+mediaQuery.addEventListener('change', adjustDevice);
+
+adjustDevice(mediaQuery);
