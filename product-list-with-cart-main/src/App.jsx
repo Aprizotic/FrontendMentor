da0c5ea7b761e916f3b904b5ec3db1ab3f2cd3fb
Aprizotic <kajusbut24@gmail.com>
2026-01-30 16:24:13 +0000

    complete static mobile

--- product-list-with-cart-main/src/App.jsx
+++ product-list-with-cart-main/src/App.jsx
@@ -1,6 +1,20 @@
-import './App.scss'
+import "./App.scss";
+import productData from "./assets/data/data.json";
+import ProductCard from "./components/ProductCard";
+import Cart from "./components/Cart";
 
 function App() {
+  return (
+    <main className="main">
+      <h1 className="main__heading">Desserts</h1>
+
+      {productData.map((product) => (
+        <ProductCard key={product.name} product={product} />
+      ))}
+
+      <Cart />
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ product-list-with-cart-main/src/components/Cart.jsx
@@ -0,0 +1,17 @@
+function Cart() {
+  return (
+    <section className="cart">
+      <h2 className="cart__heading">Your Cart (0)</h2>
+
+      <div className="cart__empty-wrapper">
+        <img
+          className="cart__img"
+          src="./assets/images/illustration-empty-cart.svg"
+        />
+        <p className="cart__description">Your added items will appear here</p>
+      </div>
+    </section>
+  );
+}
+
+export default Cart;
