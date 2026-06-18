67eceb9937907a1a83dfcf2faf609137ad2c8885
Aprizotic <kajusbut24@gmail.com>
2026-06-18 19:36:39 +0100

    feat: add basic layout

--- foreign-exchange-checker/source-code/src/App.tsx
+++ foreign-exchange-checker/source-code/src/App.tsx
@@ -1,6 +1,15 @@
 function App() {
   return (
     <>
+      <header className="header">
+        <div className="header__details"></div>
+        <section className="live-markets"></section>
+      </header>
+
+      <main className="main">
+        <section className="converter"></section>
+        <section className="stats"></section>
+      </main>
     </>
   )
 }
